caf4c03cd21e66d7e434c352c11980e9d2a2504f
gorbatschow <gorbatschow.r@gmail.com>
2023-01-08 22:14:43 +0300

    add MultiValueElement inmplementation

--- include/ImwSpinBoxAB.h
+++ include/ImwSpinBoxAB.h
@@ -4,37 +4,13 @@
 #include <array>
 
 namespace Imw {
-template <class T> class SpinBoxAB : public ValueElement<std::array<T, 2>> {
+template <class T> class SpinBoxAB : public MultiValueElement<T, 2> {
 public:
-  SpinBoxAB(const std::string &label = {})
-      : ValueElement<std::array<T, 2>>(label) {}
-
-  virtual ~SpinBoxAB() override {}
-
-  inline void setValueLimitsA(const std::pair<T, T> &limits) {
-    ValueElement<std::array<T, 2>>::_valueLimits.first[0] = limits.first;
-    ValueElement<std::array<T, 2>>::_valueLimits.second[0] = limits.second;
-  }
-
-  inline void setValueLimitsB(const std::pair<T, T> &limits) {
-    ValueElement<std::array<T, 2>>::_valueLimits.first[1] = limits.first;
-    ValueElement<std::array<T, 2>>::_valueLimits.second[1] = limits.second;
-  }
+  SpinBoxAB(const std::string &label = {}) : MultiValueElement<T, 2>(label) {}
+  virtual ~SpinBoxAB() override = default;
 
 protected:
-  virtual void paintElement() override {
-    paintElementImpl();
-
-    ValueElement<std::array<T, 2>>::_value[0] =
-        std::clamp<T>(ValueElement<std::array<T, 2>>::_value[0],
-                      ValueElement<std::array<T, 2>>::_valueLimits.first[0],
-                      ValueElement<std::array<T, 2>>::_valueLimits.second[0]);
-
-    ValueElement<std::array<T, 2>>::_value[1] =
-        std::clamp<T>(ValueElement<std::array<T, 2>>::_value[1],
-                      ValueElement<std::array<T, 2>>::_valueLimits.first[1],
-                      ValueElement<std::array<T, 2>>::_valueLimits.second[1]);
-  }
+  virtual void paintElement() override { paintElementImpl(); }
   void paintElementImpl() {}
 };
 
@@ -42,8 +18,8 @@ template <> inline void SpinBoxAB<int>::paintElementImpl() {
   int step{1};
   int step_fast{100};
   ImGuiInputTextFlags flags{0};
-  ImGui::InputScalarN(_label.c_str(), ImGuiDataType_S32, (void *)_value.data(),
-                      2, (void *)(step > 0 ? &step : NULL),
+  ImGui::InputScalarN(_label.c_str(), ImGuiDataType_S32, (void *)_values.data(),
+                      _values.size(), (void *)(step > 0 ? &step : NULL),
                       (void *)(step_fast > 0 ? &step_fast : NULL), NULL, flags);
 }
 } // namespace Imw
--- include/ImwValueElement.h
+++ include/ImwValueElement.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ImwBasicElement.h"
 #include <algorithm>
+#include <array>
 
 namespace Imw {
 template <class T> class IValueElement : public BasicElement {
@@ -45,4 +46,42 @@ protected:
   std::pair<T, T> _valueLimits{std::numeric_limits<T>::min() / T(2),
                                std::numeric_limits<T>::max() / T(2)};
 };
+
+template <class T, std::size_t N>
+class MultiValueElement : public IValueElement<std::array<T, N>> {
+public:
+  MultiValueElement(const std::string &label = {})
+      : IValueElement<std::array<T, N>>(label) {}
+  virtual ~MultiValueElement() override = default;
+
+  inline void setValue(const std::array<T, N> &values) override {
+    for (std::size_t i = 0; i != _values.size(); ++i) {
+      setValue(i, values.at(i));
+    }
+  }
+  inline const std::array<T, N> &value() const override final {
+    return _values;
+  }
+
+  inline void setValue(std::size_t i, const T &value) {
+    _values.at(i) = std::clamp(value, _valuesLimits.first.at(i),
+                               _valuesLimits.second.at(i));
+  }
+  inline const T &value(std::size_t i) const { return _values.at(i); }
+
+  inline void setValueLimits(std::size_t i, const std::pair<T, T> &limits) {
+    _valuesLimits.first.at(i) = limits.first;
+    _valuesLimits.second.at(i) = limits.second;
+    setValue(i, _values.at(i));
+  }
+  inline const std::pair<std::array<T, N>, std::array<T, N>> &
+  valueLimits() const {
+    return _valuesLimits;
+  }
+
+protected:
+  std::array<T, N> _values;
+  std::pair<std::array<T, N>, std::array<T, N>> _valuesLimits;
+};
+
 } // namespace Imw
