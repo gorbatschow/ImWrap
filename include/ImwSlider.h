a81a47c0f6a431fce4106c8112f3d3d21fd97150
gorbatschow <gorbatschow.r@gmail.com>
2023-01-10 15:49:30 +0300

    format implemented

--- include/ImwSlider.h
+++ include/ImwSlider.h
@@ -10,19 +10,27 @@ public:
 
   virtual ~Slider() override = default;
 
+  // Text format
+  void setTextFormat(const std::string &format) { _format = format; }
+  const std::string &textFormat() const { return _format; }
+
 protected:
   virtual void paintElement() override { paintElementImpl(); }
   void paintElementImpl() {}
+
+  std::string _format{};
 };
 
 template <> inline void Slider<int>::paintElementImpl() {
+  const char *format{_format.empty() ? nullptr : _format.c_str()};
   _changed = ImGui::SliderInt(_label.c_str(), &_value, _valueLimits.first,
-                              _valueLimits.second);
+                              _valueLimits.second, format);
 }
 
 template <> inline void Slider<float>::paintElementImpl() {
+  const char *format{_format.empty() ? nullptr : _format.c_str()};
   _changed = ImGui::SliderFloat(_label.c_str(), &_value, _valueLimits.first,
-                                _valueLimits.second);
+                                _valueLimits.second, format);
 }
 
 } // namespace Imw
